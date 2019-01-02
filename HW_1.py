09a4a5d209d69a88bcb9115206b71477ac9291b6
ShishirN <nanotyshishir@gmail.com>
2019-01-02 23:28:45 +0530

    Homework1

---
+++ HW_1.py
@@ -0,0 +1,20 @@
+# Metada for my favorite song
+"""
+This file contains metadata stored in
+variables for a particular song
+"""
+song = 'Girls Like You' #name of song
+rel = 'May 30, 2018' #Release date
+Genre = 'Pop'
+Artists = 'Maroon5 & CardiB'
+producer = 'Jason Evigan'
+dur = '3:55' #Duration of song
+album = 'Red Pill Blues'
+
+print(song)
+print(rel)
+print(Genre)
+print(Artists)
+print(producer)
+print(dur)
+print(album)
